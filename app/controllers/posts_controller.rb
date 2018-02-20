159c375ffcf892b9ed7b7b327d5233228e847f6f
VajkFreyasman <craig.linde.com>
2018-02-20 13:06:42 -0700

    fixed bug

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,33 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+  def index
+    @posts = Post.all
+  end
 
+  def new
+    @post = Post.new
+  end
 
-def index
-  @posts = Post.all
-end
+  def create
+    @post = Post.new(post_params)
 
+    if @post.save
+      redirect_to @post, notice: 'Your post was created successfully'
+    else
+      render :new
+    end
+  end
 
-def new
-  @post = Post.new
-end
+  def show
+  end
 
-def create
-  @post = Post.new(params.require(:post).permit(:date, :rationale))
+  private
 
-  @post.save
+    def post_params
+      params.require(:post).permit(:date, :rationale)
+    end
 
-  redirect_to @post
-end
-
-def show
-  @post = Post.find(params[:id])
-end
-
-
-
+    def set_post
+      @post = Post.find(params[:id])
+    end
 end
