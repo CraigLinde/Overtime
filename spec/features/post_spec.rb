39766c7f872f8ad519e7b37ff6f2c1b1a6271cd1
VajkFreyasman <craig.linde.com>
2018-03-01 16:20:57 -0700

    refactored rspec tests for deletion of posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -3,9 +3,14 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  let(:user) { FactoryBot.create(:user) }
+
+  let(:post) do
+    Post.create(date: Date.today, rationale: "Rationale", user_id: user.id)
+  end
+
   before do
-    @user = FactoryBot.create(:user)
-    login_as(@user, :scope => :user)
+    login_as(user, :scope => :user)
   end
 
   describe 'index' do
@@ -28,19 +33,14 @@ describe 'navigate' do
       expect(page).to have_content(/Rationale|content/)
     end
 
+    it 'has a scope so that only post creators can see their posts' do
+      other_user = User.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@example.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
 
-  it 'has a scope so that only post creators can see their posts' do
-    post1 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
-    post2 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
+      visit posts_path
 
-    other_user = User.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@example.com", password: "asdfasdf", password_confirmation: "asdfasdf")
-    post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
-
-    visit posts_path
-
-    expect(page).to_not have_content(/This post shouldn't be seen/)
-  end
-
+      expect(page).to_not have_content(/This post shouldn't be seen/)
+    end
   end
 
   describe 'new' do
@@ -54,11 +54,16 @@ describe 'navigate' do
 
   describe 'delete' do
     it 'can be deleted' do
-      @post = FactoryBot.create(:post)
-      @post.update(user_id: @user.id)
+      logout(:user)
+
+      delete_user = FactoryBot.create(:user)
+      login_as(delete_user, :scope => :user)
+
+      post_to_delete = Post.create(date: Date.today, rationale: 'asdf', user_id: delete_user.id)
+
       visit posts_path
 
-      click_link("delete_post_#{@post.id}_from_index")
+      click_link("delete_post_#{post_to_delete.id}_from_index")
       expect(page.status_code).to eq(200)
     end
   end
@@ -78,42 +83,6 @@ describe 'navigate' do
       click_on "Save"
 
       expect(page).to have_content("Some rationale")
-    end
-
-    it 'will have a user associated it' do
-      fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: "User Association"
-      click_on "Save"
-
-      expect(User.last.posts.last.rationale).to eq("User Association")
-    end
-  end
-
-  describe 'edit' do
-    before do
-      @edit_user = User.create(first_name: "asdf", last_name: "asdf", email: "asdfasdf@asdf.com", password: "asdfasdf", password_confirmation: "asdfasdf")
-      login_as(@edit_user, :scope => :user)
-      @edit_post = Post.create(date: Date.today, rationale: "asdf", user_id: @edit_user.id)
-    end
-
-    it 'can be edited' do
-      visit edit_post_path(@edit_post)
-
-      fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: "Edited content"
-      click_on "Save"
-
-      expect(page).to have_content("Edited content")
-    end
-
-    it 'cannot be edited by a non authorized user' do
-      logout(:user)
-      non_authorized_user = FactoryBot.create(:non_authorized_user)
-      login_as(non_authorized_user, :scope => :user)
-
-      visit edit_post_path(@edit_post)
-
-      expect(current_path).to eq(root_path)
     end
   end
 end
