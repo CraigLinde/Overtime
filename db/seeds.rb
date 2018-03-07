8bb235ad314f172212da2a3410f861c8da40f764
VajkFreyasman <craig.linde.com>
2018-03-07 14:22:44 -0700

    created a button for confirming overtime

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,8 +30,10 @@ end
 
 puts "100 posts were created"
 
-100.times do
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-end
 
-puts "100 Audit Logs were created"
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+
+
+puts "3 Audit Logs were created"
